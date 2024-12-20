658115c47b7d2d8a4fd196464d27d5ae3e49251c
Temur200511 <tziyodullayev61@gmail.com>
2024-12-20 10:46:34 +0500

    full

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-const sonlar = {
-    a:2,
-    b:3,
-    c:4
-}
-const n = 3;
+// const n = 3;
+// const obj = { a: 2, b: 3, c: 4 };
+// const res = Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, value * n]));
+// console.log(res);
 
-console.log(sonlar.n*(a, b,c));
+
+
+
+// const books = [
+//     { title: "Halqa", author: "Akrom Malik", read: false },
+//     { title: "Dunyoning ishlari", author: "O‘tkir Hoshimov", read: true },
+//     { title: "Iymon", author: "Shayx Muhammad Sodiq Muhammad Yusuf", read: true }
+//   ];
+  
+//   books.forEach(book => {
+//     const status = book.read ? "o‘qilgan" : "o‘qilmagan";
+//     console.log(`${book.author} ning ${book.title} kitobi ${status}`);
+//   });
+  
+
+
+
+// const animals = ['dog', 'chicken', 'cat', 'dog', 'chicken', 'chicken', 'rabbit'];
+// const count = animals.reduce((acc, animal) => {
+//   acc[animal] = (acc[animal] || 0) + 1;
+//   return acc;
+// }, {});
+// console.log(count);
+
+
+
+
+
+
+
+
